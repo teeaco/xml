ff00c7359ba12501874fc26d6477298cb4f013ec
teeaco <beihiteeaco@gmail.com>
2025-05-11 18:04:33 +0300

    mama ooooooooooooh

---
+++ example-nestjs/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { StocksModule } from './stocks/stocks.module';
+
+@Module({
+  imports: [StocksModule],
+})
+export class AppModule {}
---
+++ example-nestjs/src/stocks/dto/create-stock.dto.ts
@@ -0,0 +1,6 @@
+export class CreateStockDto {
+    src: string;
+    title: string;
+    text: string;
+  }
+  
